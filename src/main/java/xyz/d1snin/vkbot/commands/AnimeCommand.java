568a2a0aaa7b249b0b22863e2ce796875f7b7190
Michael <d1snin@aol.com>
2021-03-07 20:32:10 +0300

    added danbooru image search

--- src/main/java/xyz/d1snin/vkbot/commands/AnimeCommand.java
+++ src/main/java/xyz/d1snin/vkbot/commands/AnimeCommand.java
@@ -4,26 +4,46 @@ import ru.codeoff.bots.keyboard.Button;
 import ru.codeoff.bots.keyboard.ButtonColor;
 import ru.codeoff.bots.keyboard.Keyboard;
 import ru.codeoff.bots.sdk.objects.Message;
+import sg4e.danbooru.Danbooru;
+import sg4e.danbooru.DanbooruBuilder;
+import sg4e.danbooru.Post;
 import xyz.d1snin.vkbot.App;
 import xyz.d1snin.vkbot.util.AnimeList;
 
+import java.util.List;
+import java.util.Random;
+
 public class AnimeCommand {
     public AnimeCommand() {
-        Keyboard keys = Keyboard.of(new Button("Anime", ButtonColor.POSITIVE));
-        App.group.onCommand("Anime".toLowerCase(), message ->
+        Keyboard keys = Keyboard.of(new Button("Anime Random", ButtonColor.POSITIVE), new Button("Anime Danbooru", ButtonColor.POSITIVE));
+        App.group.onCommand(new String[]{"Anime Random".toLowerCase(), "Anime Danbooru".toLowerCase()}, message ->
                 new Message()
                 .from(App.group)
                 .to(message.authorId())
                 .text("Fetching picture...")
+                .keyboard(keys)
                 .send()
         );
-        App.group.onCommand("Anime".toLowerCase(), message ->
+        App.group.onCommand("Anime Random".toLowerCase(), message ->
                 new Message()
                 .from(App.group)
                 .to(message.authorId())
                 .photo(AnimeList.getRandomPic())
-                .keyboard(keys)
                 .send()
         );
+        App.group.onCommand("Anime Danbooru".toLowerCase(), message ->
+                new Message()
+                .from(App.group)
+                .to(message.authorId())
+                .photo(generateImage())
+                .send()
+        );
+    }
+    private static String generateImage() {
+        Danbooru danbooru = new DanbooruBuilder().build();
+        List<Post> posts = danbooru.getPosts("", true);
+        Random rand = new Random();
+        Post randomElement = posts.get(rand.nextInt(posts.size()));
+        return randomElement.getFileUrl();
     }
 }
